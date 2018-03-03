bafa6545d772152d6103f7c16c433e06b7061655
Platon Efimov <platon.efimov@gmail.com>
2018-03-03 08:23:15 +0700

    HyperlinkedModelSerializer

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,48 @@
-import rest_framework
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers, viewsets, serializers
+
+from bots.models import Message, Channel, Bot
+
+
+class BotSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Bot
+        fields = ('name', 'username')
+
+
+class BotViewSet(viewsets.ModelViewSet):
+    queryset = Bot.objects.all()
+    serializer_class = BotSerializer
+
+
+class ChannelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Channel
+        fields = ('title', 'username', 'bot')
+
+
+class ChannelViewSet(viewsets.ModelViewSet):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('channel', 'text', 'image', 'send_time')
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'bots', BotViewSet)
+router.register(r'channels', ChannelViewSet)
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
-    path('auth/', rest_framework)
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
