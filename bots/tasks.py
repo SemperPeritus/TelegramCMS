d4aa49664a4aa731d721824631281afd2db0bb91
Platon Efimov <platon.efimov@gmail.com>
2018-03-11 07:31:59 +0700

    F UI A

--- bots/tasks.py
+++ bots/tasks.py
@@ -3,11 +3,6 @@ from celery import shared_task
 
 
 @shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
 def send_message(token, channel_id, message_text, message_image_path):
     bot = telegram.Bot(token=token)
     if message_image_path is None:
